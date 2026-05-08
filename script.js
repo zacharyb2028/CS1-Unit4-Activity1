f4f2abf704a0010a81c6b6ddeea1000f33cb4fa2
zacharyb2028 <zachary.belitz2028@gbwl.org>
2026-05-08 19:03:18 +0000

    customization

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const body = document.querySelector("body");
 const heading = document.querySelector("#main-heading");
 const description = document.querySelector("#description");
 const toggleBtn = document.querySelector("#toggle");
+const image = document.querySelector("#image");
 
 // Now we can change styles & attributes for those elements
 heading.style.color = "navy"; //JS wrote CSS code...
@@ -23,19 +24,23 @@ function toggleMode() {
     // 2. Conditionally apply styles based on mode
     if (darkMode == true) {
         console.log("apply dark mode styles");
-        body.style.background = "black";
+        body.style.background = "linear-gradient(112.1deg, rgb(32, 38, 57) 11.4%, rgb(63, 76, 119) 70.2%)";
         heading.style.color = "lightgrey";
-        description.style.color = "white"
-        description.textContent = "Welcome to the dark side"
-        toggleBtn.textContent = "Switch to Light"
+        description.style.color = "white";
+        description.textContent = "Welcome to the dark side";
+        toggleBtn.textContent = "Switch to Light";
+        image.src = "DarkIMG.webp"
+        body.style.fontFamily = "comic sans";
     }
     else {
         console.log("apply light mode styles");
-        body.style.background = "white";
+        body.style.background = "linear-gradient(135deg, #fdfcfb 0%, #e2d1c3 100%)";
         heading.style.color = "navy";
         description.style.color = "black";
         description.textContent = "Welcome to the light side";
         toggleBtn.textContent = "Switch to dark";
+        image.src = "LightIMG.jpeg";
+        body.style.fontFamily = "comic sans";
     }
 }
 // Attatch the function to the button
